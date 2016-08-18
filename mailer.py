27291ca15f268461de0c3f349744139f70b87c36
Corey Mosher <corey.mosher@gmail.com>
2016-08-18 08:10:03 -0300

    Add configuration validation. Fixed up interpolation in log messages

--- mailer.py
+++ mailer.py
@@ -1,11 +1,24 @@
 import smtplib, logging
+from config_exception import ConfigException
 
 class Mailer:
     def __init__(self, config):
         self.logger = logging.getLogger('MonitorApp.Mailer')
+        self.parse_config(config)
+
+    def parse_config(self, config):
+        self.validate_config(config)
         self.smtpserver = config['smtphost']
         self.to_address = config['to_address']
         self.from_address = config['from_address']
+
+    def validate_config(self, config):
+        if ('smtphost' not in config):
+            raise ConfigException("Mailer config missing 'smtphost'")
+        if ('to_address' not in config):
+            raise ConfigException("Mailer config missing 'to_address'")
+        if ('from_address' not in config):
+            raise ConfigException("Mailer config missing 'from_address'")
 
     def sendmail(self, subject, message):
         self.logger.info("Sending message")
--- monitor.py
+++ monitor.py
@@ -1,18 +1,41 @@
 import http.client, os, logging, socket
+from config_exception import ConfigException
 
 class Monitor:
     def __init__(self, notifier, config):
         self.logger = logging.getLogger('MonitorApp.Monitor')
-        self.service_name = config['name']
-        self.url = config['host']
-        self.port = config['port']
-        self.string_to_find = config['find_string']
-        self.timeout = config['timeout']
+        self.parse_config(config)
         self.reason = None
         self.notifier = notifier
 
+    def parse_config(self, config):
+        self.validate_config(config)
+        self.service_name = config['name']
+        self.url = config['host']
+        if 'port' not in config:
+            # default to 80
+            self.logger.info("'port' not specified in config for '%s', defaulting to 80", self.service_name)
+            self.port = 80
+        else:
+            self.port = config['port']
+        self.string_to_find = config['find_string']
+        if 'timeout' not in config:
+            # default to 20 seconds
+            self.logger.info("'timeout' not specified in config for '%s', defaulting to 20 seconds", self.service_name)
+            self.timeout = 20
+        else:
+            self.timeout = config['timeout']
+
+    def validate_config(self, config):
+        if ('name' not in config):
+            raise ConfigException("Monitor config missing 'name'")
+        if ('host' not in config):
+            raise ConfigException("Monitor config missing 'host'")
+        if ('find_string' not in config):
+            raise ConfigException("Monitor config missing 'find_string'")
+
     def check(self):
-        self.logger.info('Checking service ' + self.service_name + ' url ' + self.url)
+        self.logger.info('Checking service %s url %s port %d', self.service_name, self.url, self.port)
         try:
             connection = http.client.HTTPConnection(self.url, self.port, timeout=self.timeout)
             connection.connect()
@@ -36,34 +59,30 @@ class Monitor:
             self.set_down_state()
 
     def set_up_state(self):
-        self.logger.info(self.get_service_name() + " Up")
+        self.logger.info("%s Up", self.service_name)
         # if previously in a failed state, change it
         if self.in_failed_state():
             self.remove_token_file()
-            self.notifier.sendmail(self.get_service_name() + ' Up', self.get_service_name() + " Up!")
+            self.notifier.sendmail(self.service_name + ' Up', self.service_name + " Up!")
 
     def set_down_state(self):
-        self.logger.info(self.get_service_name() + " Down! " + self.get_reason())
+        self.logger.info("%s Down! %s", self.service_name, self.get_reason())
         if not self.in_failed_state():
             self.create_token_file()
-            self.notifier.sendmail(self.get_service_name() + ' Down', self.get_service_name() + " Down! " + self.get_reason())
-
+            self.notifier.sendmail(self.service_name + ' Down', self.service_name + " Down! " + self.get_reason())
 
     def get_reason(self):
         return self.reason
 
-    def get_service_name(self):
-        return self.service_name
-
     def create_token_file(self):
-        if not os.path.exists(self.get_service_name()):
-            open(self.get_service_name(), 'w+').close()
+        if not os.path.exists(self.service_name):
+            open(self.service_name, 'w+').close()
 
     def remove_token_file(self):
-        if os.path.exists(self.get_service_name()):
-            os.remove(self.get_service_name())
+        if os.path.exists(self.service_name):
+            os.remove(self.service_name)
 
     def in_failed_state(self):
-        if os.path.exists(self.get_service_name()):
+        if os.path.exists(self.service_name):
             return True
         return False
